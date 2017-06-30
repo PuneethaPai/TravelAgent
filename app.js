15961bc13cdae6ff0e8cec0817e6d5514e234210
Suraj Shanbhag <surajus@thoughtworks.com>
2017-06-30 14:19:59 +0530

    Added Button to summary Page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const
 const app = express();
 const ClientAccessToken='80008143ef7e426e8ae929fa186012b3'
 const ClientValidationToken = config.get('validationToken');
-
+let post_data = "%3C%3Fxml+version%3D%221.0%22+encoding%3D%22UTF-8%22+standalone%3D%22yes%22%3F%3E%3CpurchaseTicketRequest+xmlns%3D%22http%3A%2F%2Fojp.nationalrail.co.uk%2Fschemas%2FFulfilmentHandoff%22%3E%3CrequestId%3E154f7b89-14f5-40b7-b33a-a5bc2428294f%3C%2FrequestId%3E%3CoriginCrs%3EEUS%3C%2ForiginCrs%3E%3CoriginName%3E%3C%2ForiginName%3E%3CoriginNlc%3E%3C%2ForiginNlc%3E%3CdestinationCrs%3EMAN%3C%2FdestinationCrs%3E%3CdestinationName%3EManchester+Piccadilly%3C%2FdestinationName%3E%3CdestinationNlc%3E%3C%2FdestinationNlc%3E%3CoutboundJourney%3E%3CdepartureDateTime%3E2017-08-10T12%3A20%3A00.000%2B01%3A00%3C%2FdepartureDateTime%3E%3CarrivalDateTime%3E2017-08-10T14%3A28%3A00.000%2B01%3A00%3C%2FarrivalDateTime%3E%3C%2FoutboundJourney%3E%3CoutboundFares%3E%3CTicketTypeDescription%3E%3C%2FTicketTypeDescription%3E%3CtotalFare%3E2200%3C%2FtotalFare%3E%3CticketType%3EVDS%3C%2FticketType%3E%3CadultFullFare%3E%3C%2FadultFullFare%3E%3CchildFullFare%3E0%3C%2FchildFullFare%3E%3CadultDiscountFare%3E0%3C%2FadultDiscountFare%3E%3CchildDiscountFare%3E0%3C%2FchildDiscountFare%3E%3CnumberOfFullFareAdults%3E1%3C%2FnumberOfFullFareAdults%3E%3CnumberOfFullFareChildren%3E0%3C%2FnumberOfFullFareChildren%3E%3CnumberOfDiscountFareAdults%3E0%3C%2FnumberOfDiscountFareAdults%3E%3CnumberOfDiscountFareChildren%3E0%3C%2FnumberOfDiscountFareChildren%3E%3CnumberOfRailcards%3E0%3C%2FnumberOfRailcards%3E%3CrouteCode%3E00474%3C%2FrouteCode%3E%3CfareOriginNlc%3E%3C%2FfareOriginNlc%3E%3CfareDestinationNlc%3E%3C%2FfareDestinationNlc%3E%3C%2FoutboundFares%3E%3C%2FpurchaseTicketRequest%3E"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -29,7 +29,18 @@ app.get('/webhook', (req, res) => {
     res.status(403).end();
   }
 });
-
+app.get('/summary', function(req, res){
+    let options = {
+        uri : "https://m.buytickets.virgintrains.co.uk/buy/purchase_ticket_request",
+        method : 'POST',
+        formData : post_data
+    }
+    request(options, function(error, response, body){
+        console.log(body);
+        res.body = body;
+        res.redirect('https://m.buytickets.virgintrains.co.uk/journeySummaryPage');
+    })
+});
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
   if (req.body.object === 'page') {
@@ -62,7 +73,28 @@ function sendMessage(event) {
       method: 'POST',
       json: {
         recipient: {id: sender},
-        message: {text: aiText}
+        message:{attachment:{
+                       "type":"template",
+                       "payload":{
+                         "template_type":"generic",
+                         "elements":[
+                           {
+                             "title":"Do You Want to Book The Ticket",
+                             "subtitle":"The local area is due for record thunderstorms over the weekend.",
+                             "image_url":"https://www.thetrainline.com/m/public/00f6856c8fb412b329525bc1bcc2f98a.jpg",
+                             "buttons":[
+                               {
+                                 "type":"web_url",
+                                 "url":"https://53de080b.ngrok.io/summary",
+                                 "title":"Show Website"
+                               }
+                             ]
+                           }
+                         ]
+                       }
+                     }
+        }
+//        message: {text: aiText}
       }
     }, (error, response) => {
       if (error) {
