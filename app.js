0211cbca69211d89d1e5cf4ba44159f3c075f022
Puneetha Pai B P <puneethp@thoughtworks.com>
2017-07-05 14:47:43 +0530

    Extract facebook reply to new file

--- app.js
+++ app.js
@@ -4,19 +4,16 @@ const
   bodyParser = require('body-parser'),
   config = require('config'),
   express = require('express'),
-  request = require('request'),
   data=require('./src/data.js'),
-  journeyList=require('./src/journeyListView.js'),
-  apiWebHook = require('./src/ApiWebHook.js');
+  apiWebHook = require('./src/ApiWebHook.js'),
+  facebookReply = require('./src/FacebookReply.js');
 
 const
     fastrackSummaryDetails = apiWebHook.fastrackSummaryDetails,
-    listViewDetails = apiWebHook.listViewDetails,
-    a = apiWebHook.a,
-    apiWebHookHandler = apiWebHook.apiWebHookHandler;
+    apiWebHookHandler = apiWebHook.apiWebHookHandler,
+    sendMessage = facebookReply.sendMessage;
 
 const app = express();
-const ClientAccessToken='80008143ef7e426e8ae929fa186012b3'
 const ClientValidationToken = config.get('validationToken');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -62,83 +59,3 @@ app.post('/webhook', (req, res) => {
 });
 
 app.post('/ai', apiWebHookHandler);
-
-const apiaiApp = require('apiai')(ClientAccessToken);
-function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
-  console.log("Sender: " + sender + "; Mesage: " + text);
-  let apiai = apiaiApp.textRequest(text, {
-    sessionId: 'tabby_cat' // use any arbitrary id
-  });
-
-  apiai.on('response', (response) => {
-  let aiText = response.result.fulfillment.speech;
-  let response_json={};
-  let custonResponse={};
-  if (aiText==="Schedule"){
-      console.log("Fix Schedule List");
-      console.log(listViewDetails.journeyList.length);
-        custonResponse={attachment: {
-                               "type": "template",
-                               "payload": {
-                                   "template_type": "list",
-                                   "top_element_style": "compact",
-                                   "elements": journeyList.showJourneyList(listViewDetails.journeyList)
-                               }
-                           }
-                           }
-  }
-  else if(aiText==="Confirm"){
-        custonResponse={
-        attachment:{
-                               "type":"template",
-                               "payload":{
-                                 "template_type":"generic",
-                                 "elements":[
-                                   {
-                                     "title":"Do You Want to Book The Ticket",
-                                     "subtitle":"The local area is due for record thunderstorms over the weekend.",
-                                     "image_url":"https://www.thetrainline.com/m/public/00f6856c8fb412b329525bc1bcc2f98a.jpg",
-                                     "buttons":[
-                                       {
-                                         "type":"web_url",
-                                         "url":"https://53de080b.ngrok.io/summary",
-                                         "title":"Book"
-                                       }
-                                     ]
-                                   }
-                                 ]
-                               }
-                             }
-                }
-
-        }
-  else{
-    custonResponse= {text:aiText }
-  }
-      request({
-          url: 'https://graph.facebook.com/v2.6/me/messages',
-          qs: {access_token: 'EAAPfT94PkckBAPqY1ZAFgtMecs7hRQnF4bgh7yu1xeBft4pKx7wVgwldZCangBx6PYPInwwTkL6ZBaL64gLCT7PBrwyqBllS2eYnv2eJBGRgMZAKnh1X8volYUaaCDPZCnLVLAcalF9EV96VLVlG8iGQuqZAQey8dqk0zDLyROMgZDZD'},
-          method: 'POST',
-          json: {
-              whitelisted_domains:["https://peterssendreceiveapp.ngrok.io"],
-              recipient: {id: sender},
-              message: custonResponse
-          }
-      }, (error, response) => {
-          if (error) {
-              console.log('Error sending message: ', error);
-          } else if (response.body.error) {
-              console.log('Error: ', response.body.error);
-          }
-      });
-
- });
-
-  apiai.on('error', (error) => {
-    console.log(error);
-  });
-
-  apiai.end();
-}
---
+++ src/FacebookReply.js
@@ -0,0 +1,92 @@
+const
+    ClientAccessToken='80008143ef7e426e8ae929fa186012b3',
+    apiaiApp = require('apiai')(ClientAccessToken),
+    journeyList=require('./journeyListView.js'),
+    request = require('request'),
+    apiWebHook = require('./ApiWebHook.js');
+
+let
+    listViewDetails = apiWebHook.listViewDetails;
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+  console.log("Sender: " + sender + "; Mesage: " + text);
+  let apiai = apiaiApp.textRequest(text, {
+    sessionId: 'tabby_cat' // use any arbitrary id
+  });
+
+  apiai.on('response', (response) => {
+  let aiText = response.result.fulfillment.speech;
+  let response_json={};
+  let custonResponse={};
+  if (aiText==="Schedule"){
+      console.log("Fix Schedule List");
+      console.log(listViewDetails.journeyList.length);
+        custonResponse={attachment: {
+                               "type": "template",
+                               "payload": {
+                                   "template_type": "list",
+                                   "top_element_style": "compact",
+                                   "elements": journeyList.showJourneyList(listViewDetails.journeyList)
+                               }
+                           }
+                           }
+  }
+  else if(aiText==="Confirm"){
+        custonResponse={
+        attachment:{
+                               "type":"template",
+                               "payload":{
+                                 "template_type":"generic",
+                                 "elements":[
+                                   {
+                                     "title":"Do You Want to Book The Ticket",
+                                     "subtitle":"The local area is due for record thunderstorms over the weekend.",
+                                     "image_url":"https://www.thetrainline.com/m/public/00f6856c8fb412b329525bc1bcc2f98a.jpg",
+                                     "buttons":[
+                                       {
+                                         "type":"web_url",
+                                         "url":"https://53de080b.ngrok.io/summary",
+                                         "title":"Book"
+                                       }
+                                     ]
+                                   }
+                                 ]
+                               }
+                             }
+                }
+
+        }
+  else{
+    custonResponse= {text:aiText }
+  }
+      request({
+          url: 'https://graph.facebook.com/v2.6/me/messages',
+          qs: {access_token: 'EAAPfT94PkckBAPqY1ZAFgtMecs7hRQnF4bgh7yu1xeBft4pKx7wVgwldZCangBx6PYPInwwTkL6ZBaL64gLCT7PBrwyqBllS2eYnv2eJBGRgMZAKnh1X8volYUaaCDPZCnLVLAcalF9EV96VLVlG8iGQuqZAQey8dqk0zDLyROMgZDZD'},
+          method: 'POST',
+          json: {
+              whitelisted_domains:["https://peterssendreceiveapp.ngrok.io"],
+              recipient: {id: sender},
+              message: custonResponse
+          }
+      }, (error, response) => {
+          if (error) {
+              console.log('Error sending message: ', error);
+          } else if (response.body.error) {
+              console.log('Error: ', response.body.error);
+          }
+      });
+
+ });
+
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
+
+  apiai.end();
+}
+
+module.exports = {
+    sendMessage : sendMessage
+}
